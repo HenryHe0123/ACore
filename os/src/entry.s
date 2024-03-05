eaa51a4ba1d0837412e1b7cafe026efc94d4df48
Henry He <119676880+HenryHe0123@users.noreply.github.com>
2024-03-05 12:31:17 +0800

    feat(os): a raw OS enable "Hello, world!" based on RustSBI

---
+++ os/src/entry.s
@@ -0,0 +1,16 @@
+    .section .text.entry
+
+    .globl _start
+_start:
+    la sp, boot_stack_top
+    call rust_main
+    
+    
+    .section .bss.stack
+    
+    .globl boot_stack_bound
+boot_stack_bound:
+    .space 4096 * 16
+
+    .globl boot_stack_top
+boot_stack_top:
---
+++ os/src/main.rs
@@ -0,0 +1,30 @@
+#![no_std]
+#![no_main]
+#![feature(panic_info_message)]
+
+mod lang_items;
+mod sbi;
+
+#[macro_use]
+mod console;
+
+use core::arch::global_asm;
+
+global_asm!(include_str!("entry.s"));
+
+#[no_mangle]
+pub fn rust_main() -> ! {
+    clear_bss();
+    println!("Hello, world!");
+    //panic!("test panic");
+    sbi::shutdown(false);
+}
+
+fn clear_bss() {
+    extern "C" {
+        fn sbss();
+        fn ebss();
+    }
+
+    (sbss as usize..ebss as usize).for_each(|p| unsafe { (p as *mut u8).write_volatile(0) });
+}
