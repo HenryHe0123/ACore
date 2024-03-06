166b982be793fbffe94c783392dbbd91ab7b63ef
Henry He <119676880+HenryHe0123@users.noreply.github.com>
2024-03-06 15:48:02 +0800

    feat(user): add user apps

---
+++ user/src/api.rs
@@ -0,0 +1,9 @@
+use crate::syscall::*;
+
+pub fn write(fd: usize, buffer: &[u8]) -> isize {
+    sys_write(fd, buffer)
+}
+
+pub fn exit(exit_code: i32) -> isize {
+    sys_exit(exit_code)
+}
---
+++ user/src/lib.rs
@@ -0,0 +1,35 @@
+#![no_std]
+#![feature(linkage)]
+#![feature(panic_info_message)]
+
+mod syscall;
+pub mod api;
+mod lang_items;
+
+#[macro_use]
+pub mod console;
+
+use api::exit;
+
+#[no_mangle]
+#[link_section = ".text.entry"]
+pub extern "C" fn _start() -> ! {
+    clear_bss();
+    exit(main());
+    panic!("unreachable after sys_exit!");
+}
+
+#[linkage = "weak"]
+#[no_mangle]
+fn main() -> i32 {
+    panic!("Cannot find main!");
+}
+
+fn clear_bss() {
+    extern "C" {
+        fn sbss();
+        fn ebss();
+    }
+
+    (sbss as usize..ebss as usize).for_each(|p| unsafe { (p as *mut u8).write_volatile(0) });
+}
